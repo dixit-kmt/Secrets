1f1ac791680888759acb628d47383799b14b12a4
Dixit Kumawat <kkuummwwaatt@gmail.com>
2022-06-30 01:40:37 +0530

    using passport.js add cookies and sessions

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const session = require("express-session");
+const passport = require("passport")
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express();
 
@@ -11,6 +13,15 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: true,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //DB connection
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -20,9 +31,15 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+userSchema.plugin(passportLocalMongoose);
 
 //Create model
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res){
@@ -37,13 +54,51 @@ app.get("/register", function(req, res){
   res.render("register")
 });
 
-app.post("/register", function(req, res){
+app.get("/secrets", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  } else{
+    res.redirect("/login");
+  }
+});
 
-})
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.post("/login", function(req, res){
-  
-})
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    } else{
+      res.redirect("/secrets");
+    }
+  })
+});
 
 
 
